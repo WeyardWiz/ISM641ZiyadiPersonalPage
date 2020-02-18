ac9547fcecd836f84f9549fa7bc48757b3af82ed
Weyard Wiz <43695586+WeyardWiz@users.noreply.github.com>
2020-02-17 23:59:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+
+<!-- ISM641 Assignment2: Home page -->
+
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title>Ziyadi's Personal Home page!</title>
+		<link rel="stylesheet" type="text/css" href="style1.css">
+	</head>
+<div style="background-image: url('https://umdearborn.edu/sites/default/files/footer-logo/cob-footer.png');background-repeat: no-repeat;background-size: cover;">	
+	<body>
+		<h3> Welcome to Mohammed Al-Ziyadi's Homepage!</h3>
+		<img src = "ziyadi.JPG" width = "120" height = "150" alt = "ziyadi">
+		<p><b> About Me: </b></p>
+		<p> My name is Mohammed Al-Ziyadi and I am an MBA/MIS student at the University of Michigan - Dearborn. 
+			<br>I am currently a self-employed IT Project Manager, with serveral industrial experiences that complement my skills!</br>
+		</p>
+			<h4> Check out my <a href = "https://www.linkedin.com/in/mohammed-alziyadi-993444100/" target="_blank">Linkedin</a> page for more info!</h4>
+			<h4> Find an HTML version of my Resume here: <a href = "ZiyadiHTMLResume.html" target="_blank">Ziyadi HTML Resume</a></h4>
+			<h4> Feel free to contact me at the following email address: <a href="mailto:malziyad@umich.edu">malziyad@umich.edu</a></h4>
+			
+			<p><b> Business courses I've taken so far: </b></p>
+			 <ul>
+			 <li> Finance 531: Corporate Finance </li>
+			 <li> Business Administration 605: Managerial Decision Making </li>
+			 </ul>
+			
+			<p><b> Business courses I'm taking right now: </b></p>
+			 <ul>
+			 <li> Information Systems Management (ISM) 641: Enterprise Architecture Network </li>
+			 <li> Business Policy & Strategy (BPS) 585: Managing Strategic Innovation & Change </li>
+			 </ul>
+			
+			 <table>
+			 <thead>
+				<tr>
+					<th rowspan = "2">
+						<img src = "https://i.pinimg.com/originals/9e/aa/09/9eaa09c8ba411a90d392478d7185ecb5.png" width = "205"
+							height = "167" alt = "U of M logo">
+					</th>
+					<th colspan = "2">
+						<strong>Business Websites I recommend </strong><br>
+					</th>
+				</tr>
+				<tr>
+					<th> Logo </th>
+					<th> Link</th>
+				</tr>
+				</thead>
+				<tbody>
+					<tr>
+					<th rowspan = "5">
+						<img src = "https://umdearborn.edu/sites/default/files/footer-logo/cob-footer.png" width = "145"
+							height = "105" alt = "COB logo">
+					</th>
+					</tr>
+					<tr>
+						<td><a href="https://www.businessinsider.com/" target="_blank"><img src="https://media.insider.com/public/assets/BI/US/logos/logos-page/BI_blue_background_vertical.png" alt="Business Insider logo" style="width:42px;height:42px;border:0;"></a></td>
+						<td><a href = "https://www.businessinsider.com/" target="_blank">Business Insider</a></td>
+					</tr>
+					
+					<tr>
+						<td><a href="https://www.wsj.com/news/business" target="_blank"><img src="https://hollanderdesign.com/app/uploads/2019/04/wsj-logo.jpg" alt="WSJ logo" style="width:42px;height:42px;border:0;"></a></td>
+						<td><a href = "https://www.wsj.com/news/business" target="_blank">Wall Street Journal</a></td>
+					</tr>
+					
+					<tr>
+						<td><a href="https://www.marketwatch.com/" target="_blank"><img src="https://mw3.wsj.net/mw5/content/logos/mw_logo_social.png" alt="Market Watch logo" style="width:42px;height:42px;border:0;"></a></td>
+						<td><a href = "https://www.marketwatch.com/" target="_blank">Market Watch</a></td>
+					</tr>
+				</tbody>
+			</table>
+			 
+			 <h4> My 3 favorite sports are: </h4>
+			 <ol>
+			 <li> Football <img src="https://cdn.lowgif.com/full/1fbb1919c9256dfc-clipart-ball-american-football-clipart-ball-american-football.gif" alt = "football gif" style="width:25px;height:25px;"></li> 
+			 <li> Hockey <img src="http://cdn.graphicsfactory.com/clip-art/image_files/image/9/1288579-hockey-006_06172006.gif" alt = "hockey gif" style="width:25px;height:25px;"></li>
+		     <li> Dodge Ball <img src="https://img.clipartlook.com/dodgeball-clipart-free-picture-dodgeball-clipart-550_480.png" alt = "dodge ball" style="width:25px;height:25px;"></li>
+			 </ol>
+			 
+			 <h4> As a video games fan, my top favorite games are: </h4>
+			 <ul>
+			 <li> Elder Scrolls Online </li>
+			 <li> Call of Duty </li>
+		     <li> Jump Force </li>
+			 </ul>
+			 
+		<h5> WebPage Designed by: Mohammed Al-ziyadi</h5>
+	</body>
+	</div>
+</html>		
